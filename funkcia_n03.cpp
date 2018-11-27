50a91a7a03e656e1d3ed65586c046502f14d059b
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2018-11-27 11:23:08 +0100

    Upravene formatovanie.

--- funkcia_n03.cpp
+++ funkcia_n03.cpp
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h> 
   
-// A linked list node 
-struct Node 
+struct zaznam 
 { 
     char kategoria[52];
 	char znacka[52];
@@ -11,318 +10,312 @@ struct Node
     int cena;
     int vyrobene;
     char stav[202];
-    struct Node *next; 
+    struct zaznam *next; 
 }; 
   
-/* Given a reference (pointer to pointer) to the head of a list 
-   and an int, inserts a new node on the front of the list. */
-void push(struct Node** head_ref, FILE *f, int *pocet_zaznamov) 
+void push(struct zaznam	**zaciatok, FILE *f, int *pocet_zaznamov) 
 { 
-	struct Node *current = NULL;
+	struct zaznam *aktualny=NULL;
 	
 	if((f=fopen("auta.txt","r"))==NULL)
 	{printf("Zaznamy neboli nacitane.\n");
 	exit(0);}
 	(*pocet_zaznamov)=0;
-    while ((current = (*head_ref)) != NULL)
+    while ((aktualny=(*zaciatok)) != NULL)
     {
-        (*head_ref) = (*head_ref)->next;
-        free(current);
+        (*zaciatok)=(*zaciatok)->next;
+        free(aktualny);
     }
-    //(*head_ref) = (struct Node*) malloc(sizeof(struct Node));
-    current=(*head_ref);
+    aktualny=(*zaciatok);
 	char s[51];
 	fseek(f,0,SEEK_SET);
 	while((fgets(s,50,f))!=NULL)
     {	
-		struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+		struct zaznam* novy_zaznam = (struct zaznam*) malloc(sizeof(struct zaznam));
 		(*pocet_zaznamov)++;
 		fgets(s,50,f);
 		s[strlen(s)-1]='\0';
-		strcpy(new_node->kategoria,s);
+		strcpy(novy_zaznam->kategoria,s);
 		fgets(s,50,f);
 		s[strlen(s)-1]='\0';
-		strcpy(new_node->znacka,s);
+		strcpy(novy_zaznam->znacka,s);
 		fgets(s,50,f);
 		s[strlen(s)-1]='\0';
-		strcpy(new_node->predajca,s);
+		strcpy(novy_zaznam->predajca,s);
 		fgets(s,50,f);
-		new_node->cena=atoi(s);
+		novy_zaznam->cena=atoi(s);
 		fgets(s,50,f);
-		new_node->vyrobene=atoi(s);
+		novy_zaznam->vyrobene=atoi(s);
 		fgets(s,50,f);
 		if(s[strlen(s)]=='\n')s[strlen(s)-1]='\0';
-		strcpy(new_node->stav,s);
-		printf("%s\n",new_node->znacka);
-	 	new_node->next=NULL;
+		strcpy(novy_zaznam->stav,s);
+	 	novy_zaznam->next=NULL;
         
-        
-        if((*head_ref)==NULL)
+        if((*zaciatok)==NULL)
         {
-        	//new_node->next = (*head_ref); 
-	    	(*head_ref)    = new_node;
-	    	current=(*head_ref);
+	    	(*zaciatok)=novy_zaznam;
+	    	aktualny=(*zaciatok);
 		}
 		else
 		{
-		 	current->next=new_node;
-	 		current=current->next;
+		 	aktualny->next=novy_zaznam;
+	 		aktualny=aktualny->next;
 		}
-	 	
-	    /*new_node->next = (*head_ref); 
-	    (*head_ref)    = new_node;*/
 	}
 	printf("Nacitalo sa %d zaznamov.\n", (*pocet_zaznamov));
 } 
 
-void add(struct Node **head_ref, int *pocet_zaznamov) 
+void add(struct zaznam **zaciatok, int *pocet_zaznamov) 
 {
 	int poz,i=1;
 	char s[202];
-	struct Node* current=(*head_ref);
+	struct zaznam* aktualny=(*zaciatok);
 	
 	scanf("%d",&poz);
 	if((*pocet_zaznamov)>0 && poz>0)
 	{
 		if(poz==1)
 		{
-			struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+			struct zaznam* novy_zaznam = (struct zaznam*) malloc(sizeof(struct zaznam)); 
 			fgets(s,200,stdin);
 			fgets(s,200,stdin);
 			s[strlen(s)-1]='\0';
-			strcpy(new_node->kategoria,s);
+			strcpy(novy_zaznam->kategoria,s);
 			fgets(s,200,stdin);
 			s[strlen(s)-1]='\0';
-			strcpy(new_node->znacka,s);
+			strcpy(novy_zaznam->znacka,s);
 			fgets(s,200,stdin);
 			s[strlen(s)-1]='\0';
-			strcpy(new_node->predajca,s);
-			scanf("%d",&new_node->cena);
-			scanf("%d",&new_node->vyrobene);
+			strcpy(novy_zaznam->predajca,s);
+			scanf("%d",&novy_zaznam->cena);
+			scanf("%d",&novy_zaznam->vyrobene);
 			fgets(s,200,stdin);
 			fgets(s,200,stdin);	
 			if(s[strlen(s)]=='\n')s[strlen(s)-1]='\0';
-			strcpy(new_node->stav,s);
+			strcpy(novy_zaznam->stav,s);
 			
-			new_node->next=(*head_ref);
-			(*head_ref)=new_node;
+			novy_zaznam->next=(*zaciatok);
+			(*zaciatok)=novy_zaznam;
 			(*pocet_zaznamov)++;
-			printf("Zaznam pridany na poziciu %d \n",i);
+			//printf("Zaznam pridany na poziciu %d \n",i);
 		}
 		else
 		{
-			while(current!=NULL)
+			while(aktualny!=NULL)
 			{
 				if(poz>(*pocet_zaznamov))
 				{
-					current = (*head_ref);
-					while(current->next!=NULL)
-						current=current->next;
-					struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+					aktualny = (*zaciatok);
+					while(aktualny->next!=NULL)
+						aktualny=aktualny->next;
+					struct zaznam* novy_zaznam = (struct zaznam*) malloc(sizeof(struct zaznam)); 
 				
 					fgets(s,200,stdin);
 					fgets(s,200,stdin);
 					s[strlen(s)-1]='\0';
-					strcpy(new_node->kategoria,s);
+					strcpy(novy_zaznam->kategoria,s);
 					fgets(s,200,stdin);
 					s[strlen(s)-1]='\0';
-					strcpy(new_node->znacka,s);
+					strcpy(novy_zaznam->znacka,s);
 					fgets(s,200,stdin);
 					s[strlen(s)-1]='\0';
-					strcpy(new_node->predajca,s);
-					scanf("%d",&new_node->cena);
-					scanf("%d",&new_node->vyrobene);
+					strcpy(novy_zaznam->predajca,s);
+					scanf("%d",&novy_zaznam->cena);
+					scanf("%d",&novy_zaznam->vyrobene);
 					fgets(s,200,stdin);
 					fgets(s,200,stdin);	
 					if(s[strlen(s)]=='\n')s[strlen(s)-1]='\0';
-					strcpy(new_node->stav,s);
+					strcpy(novy_zaznam->stav,s);
 				
-				new_node->next=current->next;
-				current->next=new_node;
+				novy_zaznam->next=aktualny->next;
+				aktualny->next=novy_zaznam;
 				(*pocet_zaznamov)++;
-				printf("Zaznam pridany na poziciu %d \n",(*pocet_zaznamov));
+				//printf("Zaznam pridany na poziciu %d \n",(*pocet_zaznamov));
 				break;
 				}
 				if((poz-1)==i++)
 				{
-				struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+				struct zaznam* novy_zaznam = (struct zaznam*) malloc(sizeof(struct zaznam)); 
 				
 				fgets(s,200,stdin);
 				fgets(s,200,stdin);
 				s[strlen(s)-1]='\0';
-				strcpy(new_node->kategoria,s);
+				strcpy(novy_zaznam->kategoria,s);
 				fgets(s,200,stdin);
 				s[strlen(s)-1]='\0';
-				strcpy(new_node->znacka,s);
+				strcpy(novy_zaznam->znacka,s);
 				fgets(s,200,stdin);
 				s[strlen(s)-1]='\0';
-				strcpy(new_node->predajca,s);
-				scanf("%d",&new_node->cena);
-				scanf("%d",&new_node->vyrobene);
+				strcpy(novy_zaznam->predajca,s);
+				scanf("%d",&novy_zaznam->cena);
+				scanf("%d",&novy_zaznam->vyrobene);
 				fgets(s,200,stdin);
 				fgets(s,200,stdin);	
 				if(s[strlen(s)]=='\n')s[strlen(s)-1]='\0';
-				strcpy(new_node->stav,s);
+				strcpy(novy_zaznam->stav,s);
 				
-				new_node->next=current->next;
-				current->next=new_node;
+				novy_zaznam->next=aktualny->next;
+				aktualny->next=novy_zaznam;
 				
 				(*pocet_zaznamov)++;
-				printf("Zaznam pridany na poziciu %d \n",i);
+				//printf("Zaznam pridany na poziciu %d \n",i);
 				break;
 				}
-				current=current->next;	
+				aktualny=aktualny->next;	
 			}
 		}
 	}
 }
   
-/* Given a reference (pointer to pointer) to the head of a list 
-   and a position, deletes the node at the given position */
-void deleteNode(struct Node **head_ref, int *pocet_zaznamov) 
+void vymaz_zaznam(struct zaznam **zaciatok, int *pocet_zaznamov) 
 { 
-   	char subs[55], doms[55];
-   	int i;
-   	struct Node* prev = *head_ref;
-   	struct Node* curr;
+   	char podstring[102], hlavny[102];
+   	int vymazane=0;
+   	struct zaznam* pred = *zaciatok;
+   	struct zaznam* aktualny;
    	
-	if (*head_ref == NULL) return;
-  	scanf("%s",subs);
-	strupr(subs);  
-	strcpy(doms,(*head_ref)->znacka);
-  	strupr(doms);
-	if(strstr(doms,subs))
+	if (*zaciatok == NULL) return;
+  	scanf("%s",podstring);
+	strupr(podstring);  
+
+	while(strstr(strupr(strcpy(hlavny,(*zaciatok)->znacka)),podstring)!=NULL)
   	{
-  		*head_ref=(*head_ref)->next;
-  		free(prev);
+  		*zaciatok=(*zaciatok)->next;
+  		free(pred);
   		(*pocet_zaznamov)--;
-  		//return;
+  		vymazane++;
 	}
-	  prev = (*head_ref);
-	  curr = prev->next;
 	
-	  while (curr != NULL) {
-	  	strcpy(doms,curr->znacka);
-  		strupr(doms);
-	    if(strstr(doms,subs)){
-	      prev->next = curr->next;  // Will be NULL at the end of the list.
-	      free(curr);
-	      curr = prev->next;
-	    } else {
-	      prev = curr;
-	      curr = curr->next;
+	  pred=(*zaciatok);
+	  aktualny=pred->next;
+	
+	  while (aktualny != NULL) {
+	  	strcpy(hlavny,aktualny->znacka);
+  		strupr(hlavny);
+	    if(strstr(hlavny,podstring))
+		{
+	      pred->next=aktualny->next;  // Will be NULL at the end of the list.
+	      free(aktualny);
+	      aktualny=pred->next;
+	      (*pocet_zaznamov)--;
+	      vymazane++;
+	    } 
+		else 
+		{
+	      pred=aktualny;
+	      aktualny=aktualny->next;
 	    }
 	  }
+	  printf("Vymazalo sa %d zaznamov.\n", vymazane);
 }
 
-void h(struct Node *node) 
+void h(struct zaznam *zaznam) 
 { 
-	struct Node *current=node;
-	int i=0, wish;
-	char subs[55], doms[55];
-	scanf("%s",subs);
-    strupr(subs);
+	struct zaznam *aktualny=zaznam;
+	int i=0, wish, check=0;
+	char podstring[102], hlavny[102];
+	scanf("%s",podstring);
+    strupr(podstring);
 	scanf("%d",&wish);
-    while (current != NULL) 
+    while (aktualny != NULL) 
     { 
-    	strcpy(doms,current->znacka);
-  		strupr(doms);
-  		printf("%s\n",doms);
-  		if(strstr(doms,subs))
-  			if(wish>=(current->cena))
-		{
-    	printf("%d.\n", ++i);
-    	printf("%s\n", current->kategoria);
-    	printf("%s\n", current->znacka);
-    	printf("%s\n", current->predajca);
-        printf("%d\n", current->cena);
-		printf("%d\n", current->vyrobene);
-		printf("%s\n", current->stav);
-		}
-        current = current->next; 
+    	strcpy(hlavny,aktualny->znacka);
+  		strupr(hlavny);
+  		if(strstr(hlavny,podstring))
+  			if(wish>=(aktualny->cena))
+			{
+		    	printf("%d.\n", ++i);
+		    	printf("%s\n", aktualny->kategoria);
+		    	printf("%s\n", aktualny->znacka);
+		    	printf("%s\n", aktualny->predajca);
+		        printf("%d\n", aktualny->cena);
+				printf("%d\n", aktualny->vyrobene);
+				printf("%s\n", aktualny->stav);
+				check=1;
+			}
+        aktualny = aktualny->next; 
     }
-    printf("\n");
+    if(check==0)printf("V ponuke nie su pozadovane auta\n");
 } 
 
-void a(struct Node **node) 
+void a(struct zaznam **zaznam) 
 { 
-	struct Node *current=(*node);
-	int i=0, rok;
-	char subs[55], doms[55];
+	struct zaznam *aktualny=(*zaznam);
+	int i=0, rok, upravene=0;
+	char podstring[102], hlavny[102];
 	
-	scanf("%s",subs);
-    strupr(subs);
+	scanf("%s",podstring);
+    strupr(podstring);
 	scanf("%d",&rok);
 	
-    while (current != NULL) 
+    while (aktualny != NULL) 
     { 
-    	strcpy(doms,current->znacka);
-  		strupr(doms);
-  		printf("%s\n",doms);
-  		if(strstr(doms,subs)!=NULL)
+    	strcpy(hlavny,aktualny->znacka);
+  		strupr(hlavny);
+  		printf("%s\n",hlavny);
+  		if(strstr(hlavny,podstring)!=NULL)
   		{
-  			if(rok==(current->vyrobene))
+  			if(rok==(aktualny->vyrobene))
   			{
-				current->cena-=100;  				
+				aktualny->cena-=100;  		
+				upravene++;
+				if(aktualny->cena<=0) aktualny->cena=0;		
 			}
 		}
-        current = current->next; 
+        aktualny = aktualny->next; 
     }
-    printf("\n");
+    printf("Aktualizovalo sa %d zaznamov,\n", upravene);
 } 
 
-// This function prints contents of linked list starting from 
-// the given node 
-void printList(struct Node *node) 
+void printList(struct zaznam *zaciatok) 
 { 
-	struct Node *current=node;
+	struct zaznam *aktualny=zaciatok;
 	int i=0;
-    while (current != NULL) 
+    while (aktualny != NULL) 
     { 
     	printf("%d.\n", ++i);
-    	printf("kategoria: %s\n", current->kategoria);
-    	printf("znacka: %s\n", current->znacka);
-    	printf("predajca: %s\n", current->predajca);
-        printf("cena: %d\n", current->cena);
-		printf("rok_vyroby: %d\n", current->vyrobene);
-		printf("stav_vozidla: %s\n", current->stav);  
-        current = current->next; 
+    	printf("kategoria: %s\n", aktualny->kategoria);
+    	printf("znacka: %s\n", aktualny->znacka);
+    	printf("predajca: %s\n", aktualny->predajca);
+        printf("cena: %d\n", aktualny->cena);
+		printf("rok_vyroby: %d\n", aktualny->vyrobene);
+		printf("stav_vozidla: %s\n", aktualny->stav);  
+        aktualny = aktualny->next; 
     }
     printf("\n");
 } 
 
-void freeAll(struct Node *head, FILE *f, int *w)
+void ukonci(struct zaznam *zaciatok, FILE *f, int *w)
 {
-    struct Node *current = NULL;
+    struct zaznam *aktualny = NULL;
 
-    while ((current = head) != NULL)
+    while ((aktualny = zaciatok) != NULL)
     {
-        head = head->next;
-        free(current);
+        zaciatok = zaciatok->next;
+        free(aktualny);
     }
     if(f!=NULL)
     fclose(f);
     *w=0;
 }
  
-/* Drier program to test above functions*/
 int main() 
 { 
-    struct Node* head = NULL, *new_node=NULL; 
+    struct zaznam* zaciatok = NULL, *novy_zaznam=NULL; 
     FILE *f=NULL;
-    char hs[51];
+    char hs[5];
     int w=1, pocet_zaznamov=0;
     
 	while(w==1)
 	{
 		scanf("%s",hs);
-		if(hs[0]=='n') push(&head, f, &pocet_zaznamov);
-		if(hs[0]=='p') add(&head, &pocet_zaznamov);  
-		if(hs[0]=='v') printList(head);
-		if(hs[0]=='h') h(head);
-		if(hs[0]=='a') a(&head);
-		if(hs[0]=='z') deleteNode(&head, &pocet_zaznamov);
-		if(hs[0]=='k') freeAll(head,f,&w);
+		if(hs[0]=='n') push(&zaciatok, f, &pocet_zaznamov);
+		if(hs[0]=='p') add(&zaciatok, &pocet_zaznamov);  
+		if(hs[0]=='v') printList(zaciatok);
+		if(hs[0]=='h') h(zaciatok);
+		if(hs[0]=='a') a(&zaciatok);
+		if(hs[0]=='z') vymaz_zaznam(&zaciatok, &pocet_zaznamov);
+		if(hs[0]=='k') ukonci(zaciatok,f,&w);
 	}
     
     return 0; 
